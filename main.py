b713f88540a7521b6e5c31c4b78aca6b704134a6
Yahor Bagatyrevich <egorbgtrvch@gmail.com>
2023-01-07 23:10:44 +0300

    added the shorter command /weather with location, predefined by user

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import aiogram.utils.markdown
+import ast
 import httpx
 
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from config.config import weather_api_config, bot_config
 
@@ -13,34 +18,74 @@ weather_api_key = weather_api_config.KEY
 bot_token = bot_config.TOKEN
 
 bot = Bot(bot_token)
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
 
-@dp.message_handler(commands=['start'])
+class UserState(StatesGroup):
+    location = State()
+
+
+@dp.message_handler(state="*", commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply("""Привет!
 Отправь мне своё местоположение для получения прогноза погоды!
 Для этого открой 'Вложения -> Геопозиция' и нажми 'Отправить свою геопозицию'.""")
 
 
-@dp.message_handler(content_types=['location'])
+@dp.message_handler(state="*", commands=["set_location"])
+async def define_constant_location(message: types.Message):
+    await UserState.location.set()
+    await message.reply("Отправьте геолокацию, которая будет стандартной для команды /weather.")
+
+
+@dp.message_handler(state=UserState.location, content_types=["location"])
+async def process_location(message: types.Message, state: FSMContext):
+    await state.set_state(state=str((message.location.latitude, message.location.longitude)))
+    await message.reply(f"Геолокация для /weather установлена.")
+
+
+@dp.message_handler(state=UserState.location)
+async def process_invalid_location(message: types.Message, state: FSMContext):
+    await message.reply(f"Неверный формат ответа. Пожалуйста, отправьте геолокацию.")
+
+
+@dp.message_handler(state='*', commands=["weather"])
+async def get_predefined_location_forecast(message: types.Message):
+    state = await dp.current_state(user=message.from_user.id).get_state()
+    if state is None:
+        await message.reply("""Нельзя использовать сокращенную команду /weather,
+т.к. не было задано стандартное местоположение. Для того, чтобы задать местоположение, 
+используйте команду /set_location""")
+        return
+
+    lat, lon = ast.literal_eval(state)
+    forecast = await get_forecast(lat, lon)
+    await message.reply(forecast)
+
+
+@dp.message_handler(state="*", content_types=['location'])
 async def handle_location(message: types.Message):
     lat = message.location.latitude
     lon = message.location.longitude
 
-    async with httpx.AsyncClient() as client:
-        resp = await client.get(f"https://api.openweathermap.org/data/{weather_api_version}/weather?lat={lat}&lon={lon}&appid={weather_api_key}")
-        resp_data = resp.json()
-
-    reply_message = get_forecast_message(resp_data)
-    await message.reply(reply_message)
+    forecast = await get_forecast(lat, lon)
+    await message.reply(forecast)
 
 
 def get_temp_celsius(kelvin: float):
     return int(kelvin - 273.15)
 
 
-def get_forecast_message(resp_data):
+async def get_forecast(lat, lon):
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(f"https://api.openweathermap.org/data/{weather_api_version}/weather?lat={lat}&lon={lon}&appid={weather_api_key}")
+        resp_data = resp.json()
+
+    return create_forecast_message(resp_data)
+
+
+def create_forecast_message(resp_data):
     return f"""Населенный пункт: {resp_data['name']},
 температура: {get_temp_celsius(resp_data['main']['temp'])}°C,
 ощущается как: {get_temp_celsius(resp_data['main']['feels_like'])}°C,
